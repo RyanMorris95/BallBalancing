3dfac9db6ded470760725c468884454c8a38c4b8
RyanMorris95 <morrisryanmorris@gmail.com>
2017-10-30 13:23:21 -0700

    Gui

---
+++ Src/Interface/ball_balancing_ui.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ball_balancing.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.verticalLayout_2 = QtGui.QVBoxLayout(self.centralwidget)
+        self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
+        self.verticalLayout = QtGui.QVBoxLayout()
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.horizontalLayout_7 = QtGui.QHBoxLayout()
+        self.horizontalLayout_7.setObjectName(_fromUtf8("horizontalLayout_7"))
+        self.interfaceLayout = QtGui.QHBoxLayout()
+        self.interfaceLayout.setObjectName(_fromUtf8("interfaceLayout"))
+        self.formLayout_2 = QtGui.QFormLayout()
+        self.formLayout_2.setFieldGrowthPolicy(QtGui.QFormLayout.AllNonFixedFieldsGrow)
+        self.formLayout_2.setObjectName(_fromUtf8("formLayout_2"))
+        self.interfaceLayout.addLayout(self.formLayout_2)
+        self.horizontalLayout_7.addLayout(self.interfaceLayout)
+        self.line = QtGui.QFrame(self.centralwidget)
+        self.line.setFrameShape(QtGui.QFrame.VLine)
+        self.line.setFrameShadow(QtGui.QFrame.Sunken)
+        self.line.setObjectName(_fromUtf8("line"))
+        self.horizontalLayout_7.addWidget(self.line)
+        self.imageLayout = QtGui.QHBoxLayout()
+        self.imageLayout.setObjectName(_fromUtf8("imageLayout"))
+        self.image_holder = QtGui.QLabel(self.centralwidget)
+        self.image_holder.setObjectName(_fromUtf8("image_holder"))
+        self.imageLayout.addWidget(self.image_holder)
+        self.horizontalLayout_7.addLayout(self.imageLayout)
+        self.verticalLayout.addLayout(self.horizontalLayout_7)
+        self.taskGraphLayout = QtGui.QHBoxLayout()
+        self.taskGraphLayout.setObjectName(_fromUtf8("taskGraphLayout"))
+        self.verticalLayout.addLayout(self.taskGraphLayout)
+        self.verticalLayout_2.addLayout(self.verticalLayout)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 25))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
+        self.image_holder.setText(_translate("MainWindow", "TextLabel", None))
+
